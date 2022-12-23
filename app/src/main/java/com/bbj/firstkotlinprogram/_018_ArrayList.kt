7e65e66acd9ecc9d27285d2c71145d9ab5766cae
Bruno Jullien <bruno.jullien@gmail.com>
2022-12-23 17:22:21 +0100

    changed ArrayList example

--- app/src/main/java/com/bbj/firstkotlinprogram/_018_ArrayList.kt
+++ app/src/main/java/com/bbj/firstkotlinprogram/_018_ArrayList.kt
@@ -3,16 +3,22 @@ package com.bbj.firstkotlinprogram
 fun main(args: Array<String>) {
 
     var age = ArrayList<Int>()
-    age.add(10)
-    age.add(1, 15)
-    age.add(20)
-    println("0: "+age[0])
-    println("1: "+age.get(1))
-    println("2: ${age[2]}")         //using string templates
+    age.add(10)                     //add 10 at the end (index = 0)
+    age.add(1, 15)     //add 15 in 2nd position
+    age.add(20)                     //add 20 in 3 position
+    println("0: "+age[0])           //10
+    println("1: "+age.get(1))       //15
+    println("2: ${age[2]}")         //20 ... using string templates
+    age.add(1, 14)     //add 14 at index = 1 =>
+    println("0: "+age[0])           //10
+    println("1: "+age.get(1))       //14 was inserted and others shifted
+    println("2: ${age[2]}")         //15 shifted by 1
+    println("3: ${age[3]}")         //20 shifted by 1
 
     age.remove(15)          //remove first occurence of 15
     println("0: "+age[0])           //10
-    println("1: "+age.get(1))       //20 (replacing 15)
+    println("1: "+age.get(1))       //14
+    println("2: "+age.get(2))       //20 (replacing 15)
 
     var cars = arrayListOf<String>("Opel","BMW")
     cars.add("Toyota")
